3eb22bfe0c977b254a96666e3ab285305dd0922d
Cindy Ward <cindyward@yahoo.com>
2014-08-02 23:07:53 -0700

    Successfully output 'one' when '1' is input

--- lib/numbers_in_words.rb
+++ lib/numbers_in_words.rb
@@ -21,8 +21,55 @@ def splitter(input_number_string)
 
 end
 
-def numbers_in_words
+def numbers_in_words(input_number_string)
+
+	ones_words = { 0=>"", 1=>"one", 2=>"two", 3=>"three", 4=>"four", 5=>"five", 6=>"six", 7=>"seven", 8=>"eight", 9=>"nine" }
+
+	teens_words = { 10=>"ten", 11=>"eleven", 12=>"twelve", 13=>"thirteen", 14=>"fourteen", 15=>"fifteen", 16=>"sixteen",
+					17=>"seventeen", 18=>"eighteen", 19=>"nineteen" }
+
+	tens_words = { 2=>"twenty", 3=>"thirty", 4=>"forty", 5=>"fifty", 6=>"sixty", 7=>"seventy", 8=>"eighty", 9=>"ninety" }
+
+	power_words = {100=>"hundred",1000=>"thousand", 1000000=>"million", 1000000000=>"billion", 1000000000000=>"trillion"}
+
+	puts "Welcome to the Numbers in Words Ruby method"
+	puts "\n"
+
+	# input_number_string = ""
+
+	while input_number_string != "x"
+
+		# puts "Please enter a number in numeric format that you would like translated into words."
+		# puts "The number may range between 1 and 999,999,999,999.
+		# puts "You may enter 'x' to exit the method.""
+		# puts "\n"
+	
+		# input_number_string = gets.chomp.downcase
+		if input_number_string != "x"
+			input_number = input_number_string.to_i
+			output_words_string = ""
+			if input_number <= 0 || input_number >= 1e12
+				puts "Invalid number #{input_number.to_s} entered, please try again or enter 'x' to exit the method"
+				input_number_string = "x"
+			else
+				number_2d_array = splitter(input_number_string)
+				current_magnitude = 10 ** (number_2d_array.length + 1)
+				number_2d_array.each_with_index do |power_array, power_index|
+					current_power = power_array.length - 1
+					power_array.each do |number, number_index|
+						output_words_string += ones_words.fetch(number)
+					end
+				end
+				return output_words_string
+				# puts "Input number = #{input_number_string}, output number in words = #{output_words_string}"
+				# puts "\n"
+				# input_number_string = "x"
+			end
+		end
+	end
 end
 
-# splitter("1234567890")
+# numbers_in_words("1")
+# numbers_in_words("3")
+
 
--- spec/numbers_in_words_spec.rb
+++ spec/numbers_in_words_spec.rb
@@ -4,23 +4,36 @@ require "numbers_in_words"
 describe ("splitter") do
 
 	it("turns the input '1' into [[1]]") do
-		expect(splitter('1')).to eq [[1]]
+		expect(splitter("1")).to eq [[1]]
 	end
 
 	it("turns the input '12' into [[1,2]]") do
-		expect(splitter('12')).to eq [[1,2]]
+		expect(splitter("12")).to eq [[1,2]]
 	end
 
 	it("turns the input '123' into [[1,2,3]]") do
-		expect(splitter('123')).to eq [[1,2,3]]
+		expect(splitter("123")).to eq [[1,2,3]]
 	end
 
 	it("turns the input '1234567' into [[1],[2,3,4],[5,6,7]]") do
-		expect(splitter('1234567')).to eq [[1],[2,3,4],[5,6,7]]
+		expect(splitter("1234567")).to eq [[1],[2,3,4],[5,6,7]]
 	end
 
 	it("turns the input '1234567890' into [[1],[2,3,4],[5,6,7]]") do
-		expect(splitter('1234567890')).to eq [[1],[2,3,4],[5,6,7],[8,9,0]]
+		expect(splitter("1234567890")).to eq [[1],[2,3,4],[5,6,7],[8,9,0]]
+	end
+
+end
+
+
+describe ("numbers_in_words") do
+
+	it("returns the correct string for numbers 1-9") do
+		expect(numbers_in_words("1")).to eq "one"
+	end
+
+	it("returns the correct string for numbers 1-9") do
+		expect(numbers_in_words("9")).to eq "nine"
 	end
 
 end
